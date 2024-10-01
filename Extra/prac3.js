98e874f99507f45bd00ae073707a13485f583d59
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-01 17:36:29 +0530

    update

--- Extra/prac3.js
+++ Extra/prac3.js
@@ -1,7 +1,7 @@
 let string = "IAmString"
 console.log(string[10]) //not throws an error instead it will return undefined
 
-let arr = [0,1,2,3]
+let arr = [0, 1, 2, 3]
 console.log(arr[4]) //same
 
 let obj = {
@@ -16,3 +16,17 @@ let obj = {
 console.log(Object.keys(obj)) //It returns the keys in an array form
 
 console.log(Object.keys(obj)[10]) //undefined
+
+console.log(Object.entries(obj)) //undefined
+
+let [name1, name2, name3] = Object.entries(obj) //destructuring
+console.log(name1, name2, name3)
+
+let { n1, n2, n3 } = { n1: 1, n2: 3, n3: 5 } //key name should be the same
+console.log(n1, n2, n3)
+
+// let { a:n1, b:n2, c:n3 } = { a: 1, b: 3, c: 5 } reassigning the name/alias/keys
+
+// const obj2 = { a: 1, b: 2 };
+// const { a, b } = obj2;
+// console.log(a,b)

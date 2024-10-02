4c13a11f05118551e20ac3166f1f7065063338c8
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-02 17:22:06 +0530

    update

--- Extra/prac4.js
+++ Extra/prac4.js
@@ -1,11 +1,24 @@
 // numbers
 
-const balance = new Number( 455.56 )
-console.log(balance.toFixed(1)) //455.6
+// const balance = new Number( 455.56 )
+// console.log(balance.toFixed(1)) //455.6
 
-console.log(balance.toPrecision(3)) //456
+// console.log(balance.toPrecision(3)) //456
 
-const hundreds = 10000000
-console.log(hundreds.toLocaleString())
-console.log(hundreds.toLocaleString('en-In'))
+// const hundreds = 10000000
+// console.log(hundreds.toLocaleString())
+// console.log(hundreds.toLocaleString('en-In'))
+
+// console.log(Math.PI)
+
+// console.log(Math.abs(-5)) //to positive
+// console.log(Math.round(4.4)) //round up
+// console.log(Math.floor(4.9)) //to floor/nearest previous
+// console.log(Math.ceil(4.1)) //to ceil/nearest next
+// console.log(Math.pow(2,3)) //2^3 = 8
+// console.log(Math.min(23,67,34,9,57))
+// console.log(Math.max(23,67,34,9,57))
+
+console.log(Math.random()) //It will always return the values from 0 to 1, e.g - 0.2634664125577164
+console.log(Math.random()*100)
 

00551032af659c8383e0abd539a79f8352770751
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-02 16:25:43 +0530

    update

--- Extra/prac4.js
+++ Extra/prac4.js
@@ -1,6 +1,11 @@
 // numbers
 
-const balance = new Number( 400 )
-console.log(balance.toFixed(1)) //400.0
+const balance = new Number( 455.56 )
+console.log(balance.toFixed(1)) //455.6
 
-console.log()
+console.log(balance.toPrecision(3)) //456
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString())
+console.log(hundreds.toLocaleString('en-In'))
+

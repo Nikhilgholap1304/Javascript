010d2545162d65e46de6903f722e817210659873
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-03 22:13:47 +0530

    update

--- Extra/prac5.js
+++ Extra/prac5.js
@@ -1,4 +1,4 @@
-let value = 1
+// let value = 1
 
 // const doSomething = () => {
 //   console.log('hii')
@@ -8,23 +8,39 @@ let value = 1
 //   }
 // }
 
-const Outerfunction=(callback)=>{
-  console.log("Executions before callback function getting executed")
-  callback()
-}
-
-Outerfunction(()=>{
-  console.log("callback function")
-})
-
-//therefore callback functions are used to execute asynchronously as well as if we want to handle the async functions like settimeout
-
 // doSomething(function(){
-//   console.log("bye")
-// })
+  //   console.log("bye")
+  // })
+  
+
+  // console.log(value)
+  // a = doSomething()
+  // console.log(a())
 
 
+  // const Outerfunction=(callback)=>{
+  //   console.log("Executions before callback function getting executed")
+  //   callback()
+  // }
+  
+  // Outerfunction(()=>{
+  //   console.log("callback function")
+  // })
+  
+  //therefore callback functions are used to execute asynchronously as well as if we want to handle the async functions like settimeout
 
-// console.log(value)
-// a = doSomething()
-// console.log(a())
+  
+  // const Outerfunction = (callbackfun) => {
+  //   setTimeout(()=>{
+  //     console.log("Outerfunctions Hii")
+  //   },2000)
+  //   callbackfun()
+  // }
+
+  // Outerfunction(()=>{
+  //   console.log("callback functions bye")
+  // })
+
+  //as you can see setTimeout will not stop the the execution , therefore callback will run first and outerfunctions clg will run after wards
+
+  // thats why for achieving the synchronous nature while having the asynchronous funciton we use promises
---
+++ Extra/prac6.js
@@ -0,0 +1,6 @@
+let arr = ['a','b','c','d'] //pass by reference and can be changes even if its stored inside constant variable
+
+console.log(arr)
+
+
+

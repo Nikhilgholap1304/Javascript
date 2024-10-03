774844f80c950a60147effbe2ee9fe5504486d17
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-03 23:09:32 +0530

    update

--- Extra/prac6.js
+++ Extra/prac6.js
@@ -1,6 +1,17 @@
 let arr = ['a','b','c','d'] //pass by reference and can be changes even if its stored inside constant variable
+//arrays are mutable
 
-console.log(arr)
+arr[4] = 'e'
+arr[6] = 'g'
+console.log(arr) //[ 'a', 'b', 'c', 'd', 'e', <1 empty item>, 'g' ]
+
+console.log(arr[5])//undefined
+
+console.log(typeof arr) //object
+
+for (item of arr){
+  console.log(item)
+}
 
 
 
---
+++ Extra/prac7.js
@@ -0,0 +1,11 @@
+const myData = new Date()
+
+console.log(myData.toString()) 
+console.log(myData.toISOString()) //same
+console.log(myData.toJSON()) //same
+console.log(myData.toDateString())
+console.log(myData.getTime()) //in milliseconds
+console.log(myData.toLocaleString())
+console.log(myData.getDate())
+console.log(myData.getHours())
+console.log(myData.getFullYear())

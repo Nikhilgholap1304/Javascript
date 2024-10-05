1bf33ec18b5281e8214b12f52a14d885903627d0
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-05 21:48:13 +0530

    update

--- Extra/prac9.js
+++ Extra/prac9.js
@@ -10,12 +10,14 @@ dc = ["batman", "flash", "superman"]
 // therefore we need to use the spread operator
 
 // marvel.push(...dc) //spread operator will destructure it 
+console.log("New Array : ", [...marvel, ...dc])
 //The spread operator is a feature of JavaScript introduced with ES6 that gives you access to the insides of an iterable object. An “iterable object” is anything you can iterate over item by item, such as arrays, objects literals, and strings
 // console.log(marvel)
 
-// marvel.concat()//alone it will not work or doesn't gets altered
-let a = marvel.concat(dc);
-console.log(a);
+marvel.concat()//alone it will not work or doesn't gets altered on original array
+console.log(marvel.concat(dc))
+// let a = marvel.concat(dc);
+// console.log(a);
 //[ 'spiderman', 'ironman', 'hulk', 'batman', 'flash', 'superman' ]
 
 

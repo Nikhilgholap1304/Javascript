0c13da7e78c8795d934bec3a060c5ed494fb7a4f
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-05 15:02:27 +0530

    update

--- Extra/prac9.js
+++ Extra/prac9.js
@@ -1,6 +1,14 @@
 marvel = ["spiderman", "ironman", "hulk"]
 dc = ["batman", "flash", "superman"]
 
-marvel.push(dc) 
-console.log(marvel) //[ 'spiderman', 'ironman', 'hulk', [ 'batman', 'flash', 'superman' ] ]
+// marvel.push(dc) 
+// console.log(marvel) 
+//[ 'spiderman', 'ironman', 'hulk', [ 'batman', 'flash', 'superman' ] ]
+// the whole array is getting pushed inside as a nested element
+
+// therefore we need to use the spread operator
+
+marvel.push(...dc) //spread operator will destructure it 
+//The spread operator is a feature of JavaScript introduced with ES6 that gives you access to the insides of an iterable object. An “iterable object” is anything you can iterate over item by item, such as arrays, objects literals, and strings
+console.log(marvel)
 

64c89e9d6054af533b03b9548fd732edcfa490bc
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-05 14:58:03 +0530

    update

---
+++ Extra/prac9.js
@@ -0,0 +1,6 @@
+marvel = ["spiderman", "ironman", "hulk"]
+dc = ["batman", "flash", "superman"]
+
+marvel.push(dc) 
+console.log(marvel) //[ 'spiderman', 'ironman', 'hulk', [ 'batman', 'flash', 'superman' ] ]
+

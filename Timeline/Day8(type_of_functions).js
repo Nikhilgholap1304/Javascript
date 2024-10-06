8831c6da67ae72718dd4b8e48b5c2837bc6a7643
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-06 12:27:18 +0530

    update

--- Timeline/Day8(type_of_functions).js
+++ Timeline/Day8(type_of_functions).js
@@ -36,24 +36,29 @@ const arr = a => console.log("arr fn 2", a) //if you have one parameter
 // arr(10)
 
 const arr1 = () => 100;
-console.log(arr());
+console.log(arr1());
 
 const arr2 = function () {
   return 100;
-}
+};
 
-  //!IIFE - immediately invoked function
+  //!IIFE - immediately invoked function expression
 
-  ((a, b) => {
-    console.log("IIFE")
-    console.log(a, b)
-  })(a, b)
+//semicolon at previous statement is must bcuz the javascript engine will interpret parenthesis as for function argument or method
+(() => {
+  console.log("IIFE")
+  // console.log(a, b)
+})();
+
 
 //!HOF
 
-const HOF = (a, b) => {
+const HOF = (a, b) => { //paramenter or an placeholder
   console.log("Higher order function")
   console.log(a("callback function"));
 }
 
-HOF((x) => x, 20); //!callback function
+HOF(
+  ((x) => x), 20); //!callback function
+
+//!Nested Function
--- Timeline/tempCodeRunnerFile.js
+++ Timeline/tempCodeRunnerFile.js
@@ -1,2 +1,4 @@
-;
-// console.log(confirmMessage)
+((a, b) => {
+    console.log("IIFE")
+    console.log(a, b)
+  })(a, b)

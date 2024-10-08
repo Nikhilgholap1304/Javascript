4dfd5cd32b4154b74789ed6febf5bb0517f05f75
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-08 17:10:18 +0530

    update

--- Extra/prac11.js
+++ Extra/prac11.js
@@ -1,9 +1,15 @@
 const scores = [23, 5, 79, 10, 14]
 
-const newScores = scores.filter((item) => {
-  // if(item<15)
-  //   return true
-  return item < 15;
-})
+// const newScores = scores.filter((item) => {
+//   // if(item<15)
+//   //   return true
+//   return item < 15;
+// })
 
-console.log(newScores) //[ 5, 10, 14 ]
+// console.log(newScores) //[ 5, 10, 14 ]
+
+const newScores2 = scores.reduce((preValue, curValue, curIndex)=>{
+  console.log(curIndex)
+  return preValue+curValue
+})
+console.log(newScores2) //131

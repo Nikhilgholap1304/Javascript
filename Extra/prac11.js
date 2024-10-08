a239f83e9a80b05c7e0060bb2b99f1ab056459b2
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-08 17:58:28 +0530

    update

--- Extra/prac11.js
+++ Extra/prac11.js
@@ -1,4 +1,5 @@
-// const scores = [23, 5, 79, 10, 14]
+const scores = [23, 5, 79, 10, 14]
+scores.reduce(()=>{})
 
 // const newScores = scores.filter((item) => {  
 //   // if the condition returns true then that element will be included otherwise it won't
@@ -18,35 +19,36 @@
 //trying to create the user defined reduce method as well
 
 class Arr{
-    constructor(...elements){
-      this.elements = elements
-      return this.elements
-    }
-    map(callback){
-      let newArr = []
-      for(let [i,ele] of Object.entries(this.elements)){
-        this.newArr.push(callback(ele, i, this.elements ))
-      }
-      return newArr
-    }
-    filter(callback){
-      let newArr = []
-      for(let ele of this.elements){
-        if(callback(ele, i, this.elements)){
-          newArr.push(ele)
-        }
-      }
-      return newArr
-    }
-    reduce(callback){
-      let outcome = 0
-      let e = this.elements
-      for (let i in e){
-        outcome += callback(e[i-1], e[i])
-      }
-      return outcome
-    }
+  constructor(...elements){
+    this.elements = elements
+    return this.elements
   }
+  map(callback){
+    let newArr = []
+    for(let [i,ele] of Object.entries(this.elements)){
+      this.newArr.push(callback(ele, i, this.elements ))
+    }
+    return newArr
+  }
+  filter(callback){
+    let newArr = []
+    for(let ele of this.elements){
+      if(callback(ele, i, this.elements)){
+        newArr.push(ele)
+      }
+    }
+    return newArr
+  }
+  reduce(callback){
+    let outcome = 0
+    let e = this.elements
+    for (let i in e){
+      outcome += callback(e[i-1], e[i], i, e)
+    }
+    return outcome
+  }
+}
+
 
 const arr1 = new Arr(10,20,30,40,50)
 
@@ -58,14 +60,19 @@ console.log(arr1)
 // })
 // console.log(newArr) 
 
-// const newArr2 = arr1.reduce((prev, curr)=>{ //user-defined reduce function
-//   return prev*curr
+// const newArr2 = arr1.reduce((prevVal, currVal, currIndex, arr)=>{ //user-defined reduce function
+//   // console.log(currIndex, arr)
+//   return prevVal*currVal
 // })
 
 // console.log(newArr2)
 
-const newFilteredArr = arr1.filter((item)=>{
-  return item > 30
-})
-console.log(newFilteredArr)
+// const newFilteredArr = arr1.filter((item, i)=>{
+//   return item > 30
+// })
+// console.log(newFilteredArr)
+
+
+
+
 

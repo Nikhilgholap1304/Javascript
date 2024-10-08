8684715cc1611632988319e30ac0b73ae2509d8d
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-08 17:16:05 +0530

    update

--- Extra/prac11.js
+++ Extra/prac11.js
@@ -1,6 +1,7 @@
 const scores = [23, 5, 79, 10, 14]
 
-// const newScores = scores.filter((item) => {
+// const newScores = scores.filter((item) => {  
+//   // if the condition returns true then that element will be included otherwise it won't
 //   // if(item<15)
 //   //   return true
 //   return item < 15;
@@ -8,8 +9,31 @@ const scores = [23, 5, 79, 10, 14]
 
 // console.log(newScores) //[ 5, 10, 14 ]
 
-const newScores2 = scores.reduce((preValue, curValue, curIndex)=>{
-  console.log(curIndex)
-  return preValue+curValue
-})
-console.log(newScores2) //131
+// const newScores2 = scores.reduce((preValue, curValue, curIndex)=>{
+//   console.log(curIndex)
+//   return preValue+curValue
+// })
+// console.log(newScores2) //131
+
+//trying to create the user defined reduce method as well
+class Arr{
+    constructor(...elements){
+      this.elements = elements
+      return this.elements
+    }
+    map(callback){
+      let newArr = []
+      for(let ele of this.elements){
+        // console.log("working", ele)
+        this.newArr.push(callback(ele))
+      }
+      return newArr
+    }
+    reduce(callback){
+      
+    }
+  }
+
+const arr1 = new Arr(10,20,30,40,50)
+
+console.log(arr1)

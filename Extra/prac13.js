8013eaa84a18199bbb3d1cfdf991de57112e2dde
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-09 13:51:59 +0530

    update

--- Extra/prac13.js
+++ Extra/prac13.js
@@ -8,8 +8,8 @@
 
 const obj1 = {a:1,b:2}
 const obj2 = {c:3, d:4}
-const obj3 = {obj1,obj2}
-console.log(obj3) 
+// const obj3 = {obj1,obj2}
+// console.log(obj3) 
 // { 
   // obj1: 
   //     { a: 1, b: 2 },
@@ -18,4 +18,8 @@ console.log(obj3)
 // }
 
 
-console.log({...obj1, ...obj2}) //{ a: 1, b: 2, c: 3, d: 4 }
+// console.log({...obj1, ...obj2}) //{ a: 1, b: 2, c: 3, d: 4 }
+const obj4 = {e:5,f:6}
+
+const obj5 = Object.assign(obj1, obj2, obj4)
+console.log(obj5) //{ a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 }

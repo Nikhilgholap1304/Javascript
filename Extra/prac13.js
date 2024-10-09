786c00e235089c2330646fd5510e4184aa105438
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-09 13:42:37 +0530

    update

--- Extra/prac13.js
+++ Extra/prac13.js
@@ -12,9 +12,9 @@ const obj3 = {obj1,obj2}
 console.log(obj3) 
 // { 
   // obj1: 
-  //   { a: 1, b: 2 },
+  //     { a: 1, b: 2 },
   // obj2: 
-  //   { c: 3, d: 4 },
+  //     { c: 3, d: 4 },
 // }
 
 

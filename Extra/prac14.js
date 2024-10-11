5c7f2ec43209ea0c8d309a42d3007d161d8a84d5
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-11 18:12:38 +0530

    update

--- Extra/prac14.js
+++ Extra/prac14.js
@@ -1,1 +1,10 @@
 console.log("logging message in console window")
+
+console.error("displaying error message")
+
+console.assert(2 > 3, "Its an asertion error message") //Assertion failed: Its an asertion error message
+//if condition gets false
+
+console.warn("Its an warning")
+
+console.table({ a: 1, b: 2, c: 3 })

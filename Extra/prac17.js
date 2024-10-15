638e6f8c45e0fde8f221d22089b4d6df0459a0bc
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-15 14:58:54 +0530

    update

--- Extra/prac17.js
+++ Extra/prac17.js
@@ -1,16 +1,37 @@
-const map = new Map() 
+// map object
+//The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value
+
+const map = new Map() //its also an iterable along with string and array
 //key value pairs like object, the difference is that it will no contain the duplicate Keys 
-map.set("IN", "India")
+map.set(1, "India")
 map.set("USA", "United States of America")
-map.set("FR", "France")
-map.set("AM", "France")
+map.set(true, "France")
+map.set([1,2,3], "France")
 
-console.log(map) 
+console.log(map.keys()) 
+// [Map Iterator] { 1, 'USA', true, [ 1, 2, 3 ] }
 
-for (const e of map) { //its similar to Object.entries()
-  console.log(e)
+for (let element of map.keys()) {
+  console.log(element, typeof element)
 }
+
+// 1 : India
+// USA : United States of America
+// true : France
+// [ 1, 2, 3 ] : France
+
+// for (const e of map) { //its similar to Object.entries().... 
+//   // whole key and value pairs would be as value/element inside the map
+//   console.log(e)
+// }
+// for (const [key, value] of map) { //its similar to Object.entries()
+//   console.log(key,":", value)
+// }
 // [ 'IN', 'India' ]
 // [ 'USA', 'United States of America' ]
 // [ 'FR', 'France' ]
 // [ 'AM', 'France' ]
+
+// for (const key in map) {
+//   console.log(key) //this will not work, it cannot be iterated
+// }
--- Extra/tempCodeRunnerFile.js
+++ Extra/tempCodeRunnerFile.js
@@ -1,9 +1,3 @@
-let name = "Nikhil"
-
-// const demo = () => {
-//   console.log(name)
-//   // var name = "Ayush" //undefined while console logging
-//   // let name = "Ayush" //Cannot access 'name' before initialization
-//   const name = "Ayush" //Cannot access 'name' before initialization
+ (const [key, value] of map) { //its similar to Object.entries()
+//   console.log(key,":", value)
 // }
-// demo()

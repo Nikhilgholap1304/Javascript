89582639ec15364ad8bad9474483c57a7ed49ca9
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-16 22:38:28 +0530

    update

--- problems/prob1.js
+++ problems/prob1.js
@@ -1,9 +1,10 @@
 //to find the closest number of n number from a set without using loop
 const prompt = require("prompt-sync")();
 
-let set = [5, 2, 7, 4, 1, 9]
+// let set = [5, 2, 7, 4, 1, 9]
+let set = [23, 68, 17, 57, 18, 72]
 // const n = parseInt(prompt(`Enter the index position to pick from ${set} - `))
-const n = 4
+const n = 57
 //its by default in the string therefore converting into the integer
 
 // console.log(set.includes(n))
@@ -11,12 +12,29 @@ if (!set.includes(n)) {
   throw new Error("Number is not present");
 }
 
-set.sort((a, b) => a - b)
-console.log(set)
+const getClosest = (nValue, set) => {
+  let closestVal;
 
-const Nindex = set.indexOf(n)
-console.log(Nindex)
+  set.sort((a, b) => a - b);
+  console.log(set)
 
-const closestVal = set[Nindex+1] > set[Nindex-1] ? set[Nindex+1] : set[Nindex-1]
-console.log(closestVal)
+  const nIndex = set.indexOf(nValue)
+  console.log(nIndex)
+
+  if (set[nIndex + 1] > set[nIndex - 1]) {
+    closestVal = set[nIndex + 1]
+  } else if (!set[nIndex - 1]) {
+    console.log("getting")
+    closestVal = set[nIndex + 1]
+  } else if (!set[nIndex + 1]) {
+    closestVal = set[nIndex - 1]
+  } else {
+    closestVal = set[nIndex + 1]
+  }
+
+  return closestVal
+}
+
+
+console.log(getClosest(68, set))
 

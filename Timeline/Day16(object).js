8e8458c1a29be20233ab44af8d20018340455b88
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-16 20:48:26 +0530

    update

--- Timeline/Day16(object).js
+++ Timeline/Day16(object).js
@@ -14,3 +14,39 @@ console.log(str1.trim())
 
 console.log(str1.trimStart())
 console.log(str1.trimEnd())
+
+console.log(s.toUpperCase());
+//HELLO
+console.log(r.toLowerCase());
+//react
+
+///Objects-------------------------------------------
+
+//!Literal way:
+
+const std = {
+  name: "Naughty Atul",
+  id: 68 + 1,
+  skills: ["SQL"],
+  isPartTimeEmployee: true,
+  job: ["plumber", "electrician", "police"],
+  id: 400,
+  address: {
+    pinCode: 400601,
+    location: "JSpiders thane",
+  },
+};
+
+console.log(std["skills"]);
+console.log(std.skills);
+console.log(std.address.location)
+console.log(std.marks)
+//undefined
+
+std["percentage"] = 95
+std.skills = ["JS", "REACT"];
+
+delete std["id"];
+delete std.job
+
+console.log(std)

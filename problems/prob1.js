c21b6162df7a7b97e04b35d30a2d9de8063df9e7
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-16 22:48:16 +0530

    update

--- problems/prob1.js
+++ problems/prob1.js
@@ -3,41 +3,40 @@
 const prompt = require("prompt-sync")();
 
 // let set = [5, 2, 7, 4, 1, 9]
-// const n = parseInt(prompt(`Enter the index position to pick from ${set} - `))
 //its by default in the string therefore converting into the integer
 
-// console.log(set.includes(n))
+// console.log(set.includes(nValue))
 
 const getClosest = (nValue, set) => {
-
+  
   let closestVal;
-
+  
   if (!set.includes(nValue)) {
     throw new Error("Number is not present");
   }
-
+  
   set.sort((a, b) => a - b);
   console.log(set)
-
+  
   const nIndex = set.indexOf(nValue)
   console.log(nIndex)
-
+  
   if (set[nIndex + 1] > set[nIndex - 1]) {
     closestVal = set[nIndex + 1]
   } else if (!set[nIndex - 1]) {
-    console.log("getting")
     closestVal = set[nIndex + 1]
   } else if (!set[nIndex + 1]) {
     closestVal = set[nIndex - 1]
   } else {
     closestVal = set[nIndex + 1]
   }
-
+  
   return closestVal
 }
 
 
 let set = [23, 68, 17, 57, 18, 72]
+// const nValue = parseInt(prompt(`Enter the index position to pick from ${set} - `))
 const nValue = 68
 
 console.log(getClosest(nValue, set))

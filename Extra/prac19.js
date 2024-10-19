6380c27308b76824b417013016e70ec7441ba5d0
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-19 22:09:13 +0530

    update

--- Extra/prac19.js
+++ Extra/prac19.js
@@ -1,8 +1,12 @@
-console.log(document.body)
-console.log(document.body.firstChild) //it will also select the space as a child
-console.log(document.body.lastChild)
-console.log(document.body.childNodes) //text describes the space
-//it would be in the form of nodelist
+// console.log(document.body)
+// console.log(document.body.firstChild) //it will also select the space as a child
+// console.log(document.body.lastChild)
+// console.log(document.body.childNodes) //text describes the space
+// //it would be in the form of nodelist
 
-console.log(document.childNodes[0] === document.firstChild) //true
-console.log(document.childNodes[document.childNodes.length - 1] === document.lastChild) //truef
+// console.log(document.childNodes[0] === document.firstChild) //true
+// console.log(document.childNodes[document.childNodes.length - 1] === document.lastChild) //true
+
+console.log(document.body.hasChildNodes()) //true
+
+console.log(Array.from(document.body.childNodes)) //from nodelist to js array

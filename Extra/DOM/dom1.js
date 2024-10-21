67320488d2c91e8f74212b79641192f7966fe526
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-21 18:46:21 +0530

    update

--- Extra/DOM/dom1.js
+++ Extra/DOM/dom1.js
@@ -1,4 +1,17 @@
 console.log(window.document)
 console.log(window)
 
-console.dir(document) //if we want anything from tag form to object form
+console.dir(document) //if we want show anything from tag form to object form
+
+console.log(document.baseURI)
+//http://127.0.0.1:5500/Extra/DOM/index.html
+
+console.log(document.links)
+//returns the HTMLCollection which is not same as array
+console.log(document.childNodes)
+//returns a NodeList
+
+console.log(Array.from(document.links))
+console.log(Array.from(document.childNodes))
+
+//use $0 to access the current selected element in the browser

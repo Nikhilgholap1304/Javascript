68b9220734422f0c427931715caec4d01728d89d
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-21 20:24:12 +0530

    update

--- Extra/DOM/dom1.js
+++ Extra/DOM/dom1.js
@@ -8,10 +8,33 @@ console.log(document.baseURI)
 
 console.log(document.links)
 //returns the HTMLCollection which is not same as array
-console.log(document.childNodes)
+console.log(document.childNodes) //it will consider the entire html, therefore specify body also
 //returns a NodeList
 
 console.log(Array.from(document.links))
-console.log(Array.from(document.childNodes))
+console.log(Array.from(document.body.childNodes))
+//[text, div, text, script]
 
 //use $0 to access the current selected element in the browser
+
+console.log(document.firstChild) //<!DOCTYPE html>
+console.log(document.body.firstChild )
+//#text
+
+
+console.log(document.body.lastChild )
+
+console.log(document.body.parentElement)
+//html
+console.log(document.body.parentNode)
+//same as above
+
+
+console.log(document.body.querySelector('div').nextElementSibling)
+//script
+console.log(document.body.querySelector('h1').nextElementSibling) 
+//p tag
+
+console.log(document.body.querySelector('h1').nextSibling) 
+//text (space)
+

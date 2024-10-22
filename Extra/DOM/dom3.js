71dce377f6b7cb7b27ae6ab6043c6bbcefeaab4e
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-22 19:51:17 +0530

    update

---
+++ Extra/DOM/dom3.js
@@ -0,0 +1,27 @@
+let d = document.body
+let t = d.children[0].children[0]
+// let t = d.firstElementChild.firstElementChild
+
+console.log(t.rows)
+console.log(t.rows)
+//accessing table in two ways you will get all the tr
+
+console.log(t.caption)
+//null
+
+console.log(t.tHead)
+console.log(t.tFoot)
+
+console.log(t.tBodies)
+//html collection of no of tbody used
+
+console.log(t.rows[0].cells)
+//tr > td(1cell), td(nthcell)
+
+console.log(typeof document)
+//object
+
+//Searching the DOM
+
+
+

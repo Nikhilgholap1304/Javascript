3a367d26e46da6cded51ba6f4ca4501220cda7e0
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-23 17:01:58 +0530

    update

--- Extra/DOM/dom5.js
+++ Extra/DOM/dom5.js
@@ -8,4 +8,7 @@ console.log(clg.innerHTML) //it will give as as it is even the child tags
 console.log(clg.textContent) //it will only give text even from child tags
 //will give same output if its only text but
 
-//difference
+
+console.log(document.querySelector("input[type=password]"))
+
+{/* <input type="password" name="password" id="password"> */}

d2964854b6984aa7e652dc027d2eea9a63dbdf0a
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-23 16:53:14 +0530

    update

---
+++ Extra/DOM/dom5.js
@@ -0,0 +1,11 @@
+// const clg = document.getElementsByClassName('clg')
+// console.log(clg[0].textContent)
+
+const clg = document.querySelector('.clg')
+
+console.log(clg.innerText) //only text
+console.log(clg.innerHTML) //it will give as as it is even the child tags
+console.log(clg.textContent) //it will only give text even from child tags
+//will give same output if its only text but
+
+//difference

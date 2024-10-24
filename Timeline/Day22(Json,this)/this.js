f895c496d0a62cd6c8c952b6b6caa6995e47d748
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-24 17:58:10 +0530

    update

--- Timeline/Day22(Json,this)/this.js
+++ Timeline/Day22(Json,this)/this.js
@@ -3,33 +3,38 @@
 // console.log(this===window)
 
 //! window object if not using "use strict"
-//! otherwise undefined
+//! otherwise undefined (except arrow function)
 
 function demo() {
   console.log(this);
+  //undefined if using strict syntax
 }
 demo()
 
 let f1 = function () {
   console.log(this);
+  //undefined if using strict syntax
 };
 f1()
 
 //!parent scope this keyword value:
 
 console.log(this)
-//see you are able to access the this/window in global hence....
+//see you are able to access the this/window in global despite using strict mode hence....
 let ar = () => {
   console.log("this of arrow", this);
 };
 ar();
-
 //the above this is accessing the "this" value from parent scope which is global.
 
 function f2() {
   console.log(this);
+  //undefined
+  //bcuz you have used the "this" which can't be accessed inside named function while using the strict mode 
   let arr2 = () => {
     console.log(this)
+    //undefined
+    //now when it comes to arrow function, the "this" here can only refer to "this" value of parent scope which if it gets undefined (strict mode) then so it will otherwise like window object (without strict mode) then also so it will consider same
   };
   arr2();
 }

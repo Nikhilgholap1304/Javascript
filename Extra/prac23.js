10e3f2f84929acae48de12a73460784de9fc4196
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-28 23:09:39 +0530

    update

--- Extra/prac23.js
+++ Extra/prac23.js
@@ -13,7 +13,7 @@
 
 // A “producing code” that does something and takes time. For instance, some code that loads the data over a network. That’s a “singer”.
 // A “consuming code” that wants the result of the “producing code” once it’s ready. Many functions may need that result. These are the “fans”.
-// A promise is a special JavaScript object that links the “producing code” and the “consuming code” together. In terms of our analogy: this is the “subscription list”. The “producing code” takes whatever time it needs to produce the promised result, and the “promise” makes that result available to all of the subscribed code when it’s ready.
+// A promise is a special JavaScript object that links the “producing code” and the “consuming code” together. In terms of our analogy: this is the “subscription list”. The “producing code” takes whatever time it needs to produce the promised result, and the “promise” makes that result available to all of the subscribed code when it’s ready untill then other code is executed.
 
 // let promise = new Promise(function(resolve, reject) {
 //   // executor (the producing code, "singer")
@@ -55,24 +55,88 @@
 // }, 2000);
 
 
+//producers
+
+// const p1 = new Promise((resolve, reject) => {
+//   console.log("Promise1 is pending")
+//   setTimeout(() => {
+//     console.log("I am a promise and I got resolved")
+//     resolve("done")
+//   }, 5000);
+// })
+
+// const p2 = new Promise((resolve, reject) => {
+//   console.log("Promise2 is pending")
+//   setTimeout(() => {
+//     console.log("I am a promise and I got rejected");
+//     reject("Failed")
+//   }, 5000);
+// })
+
+// console.log(p1, p2)
+
+//concurrently they would be fired
+
+// now look, this "then", finally and catch methods will pass one callback function (executer) as argument in which the resolved/rejected value is then passed to the parameter of callback which we can access 
+//we can call "this",finally,catch as consumers which has subscribed to the producer result via a promise
+
+// p1.then((value) => console.log("This is value", value));
+// This is value done
+// this will be fired soon after p1 promise gets fulfilled(resolved or rejected)
 
 
-const p1 = new Promise((resolve, reject) => {
-  console.log("Promise1 is pending")
+// p2.catch((value) => console.log("This is value", value));
+// This is value Failed
+// after the entire p1 this will be fired soon after p2 promise gets fulfilled(resolved or rejected)
+// whenever you fire/call the reject callbackFn then we need to use catch method instead of then() to handle the error
+
+// p2.then((value) => {
+//   console.log("p2 got resolved", value)
+// }, (error) => {
+//   console.log("p2 got an error(rejected)", error)
+// })
+//p2 got an error(rejected) Failed
+//fun fact, it can also pass two callback arguments
+//1) containing the call back which will fire if promise gets resolved
+//2) the other call back function will fire if promise gets rejected (so it behaves like a .catch method)
+
+// Attaches callbacks for the resolution and/or rejection of the Promise.
+// @param onfulfilled — The callback to execute when the Promise is resolved.
+// @param onrejected — The callback to execute when the Promise is rejected.
+
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     console.log("Heloooooo 1")
+//   }, 2000);
+// }).then((value)=>{
+//   console.log("Helooooooo 2", value)
+// })
+// Heloooooo 1
+
+//it will not perfom the "then" method in an pending state cuz we haven't called either resolve or reject and hence its not fulfilled to move further
+
+new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("I am a promise and I got resolved")
-    resolve("done")
-  }, 5000);
+    console.log("Heloooooo 1")
+    resolve("Awesome")
+  }, 2000);
+}).then((value)=>{
+  console.log("Helooooooo 2", value)
+}).catch((error)=>{
+  console.log("we got an error", error)
 })
+// Heloooooo 1
+// Helooooooo 2 Awesome
 
-const p2 = new Promise((resolve, reject) => {
-  console.log("Promise2 is pending")
+new Promise((resolve, reject) => {
   setTimeout(() => {
-    console.log("I am a promise and I got rejected");
-    reject()
-  }, 5000);
+    console.log("Heloooooo 1")
+    reject("Awesome")
+  }, 2000);
+}).then((value)=>{
+  console.log("Helooooooo 2", value)
+}).catch((error)=>{
+  console.log("we got an error", error)
 })
-
-console.log(p1, p2)
-
-
+// Heloooooo 1
+// we got an error Awesome

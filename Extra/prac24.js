8871ed61628ed44069afb3a3b02821dba9e91ea5
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-31 14:22:32 +0530

    update

--- Extra/prac24.js
+++ Extra/prac24.js
@@ -2,3 +2,21 @@
 
 // There are 6 static methods of Promise class
 
+let p1 = new Promise(()=>{
+  setTimeout(() => {
+    resolve("p1 promise")
+  }, 1000); 
+})
+
+let p2 = new Promise(()=>{
+  setTimeout(() => {
+    resolve("p2 promise")
+  }, 2000); 
+})
+
+let p3 = new Promise(()=>{
+  setTimeout(() => {
+    resolve("p3 promise")
+  }, 3000); 
+})
+

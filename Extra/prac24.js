5f1ff21b90c0656781cf77b556bd6ddc3c5a2deb
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-10-31 20:30:08 +0530

    update

--- Extra/prac24.js
+++ Extra/prac24.js
@@ -2,24 +2,35 @@
 
 // There are 6 static methods of Promise class
 
-let p1 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("p1 promise")
-  }, 1000);
-})
+// let p1 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("p1 promise")
+//   }, 3000);
+// })
 
-let p2 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("p2 promise")
-  }, 2000);
-})
+// let p2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("p2 promise")
+//   }, 2000);
+// })
 
-let p3 = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve("p3 promise")
-  }, 3000);
-})
+// let p3 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     reject("p3 promise")
+//   }, 4000);
+// })
 
+
+
+// Promise.all(
+//   [p1, p2, p3]
+// ).then((value) => {
+//   console.log(value)
+//   // [ 'p1 promise', 'p2 promise', 'p3 promise' ]
+// }).catch((error) => {
+//   console.log("unfortunately rejected", error)
+//   //unfortunately rejected p3 promise if even one promise reject
+// })
 
 //Creates a Promise that is resolved with an array of results when all of the provided Promises resolve, otherwise rejected when any Promise is rejected.
 
@@ -27,16 +38,60 @@ let p3 = new Promise((resolve, reject) => {
 
 // @returns — A new Promise.
 
-let p4 = Promise.all(
-  [p1, p2, p3]
-).then((value) => {
+
+// Promise.allSettled(
+//   [p1, p2, p3]
+// ).then((value) => {
+//   console.log(value)
+//   // [
+//   //   { status: 'fulfilled', value: 'p1 promise' },
+//   //   { status: 'rejected', reason: 'p2 promise' },
+//   //   { status: 'fulfilled', value: 'p3 promise' }
+//   // ]
+// })
+
+// Creates a Promise that is resolved with an array of results when all of the provided Promises resolve or reject.
+
+// @param values — An array of Promises.
+
+// @returns — A new Promise.
+
+//It will not throw any error
+//It will return the array of objects containing status, value/reason of promises whoever either got fulfilled or rejected i.e settled
+
+// Promise.race(
+//   [p1, p2, p3]
+// ).then((value) => {
+//   console.log(value)
+//   //p2 promise
+// }).catch((error) => {
+//   console.log(error)
+// })
+
+//as the name suggests (race) whichever promise settles (resolved or rejected) first will be returned as a single value from this promise and we can access this value using then() method and even if all the promises were pending it will return empty
+
+// Promise.any(
+//   [p1,p2,p3]
+// ).then((value)=>{
+//   console.log(value)
+//   //p3 promise
+// }).catch((error)=>{
+//   console.log(error)
+//   // [AggregateError: All promises were rejected] {
+//   //   [errors]: [ 'p1 promise', 'p2 promise', 'p3 promise' ]
+//   // }
+// })
+
+//This promise just want any promise to be fulfilled and same as race but the difference it it will only consider the promise which got resolved first and return single value of that promise and also if none of the promises were resolved then it will throw an aggregate error 
+
+Promise.resolve("This is an resolve value").then((value)=>{
   console.log(value)
-  // [ 'p1 promise', 'p2 promise', 'p3 promise' ]
-}).catch((error) => {
-  console.log("unfortunately rejected", error)
-  //unfortunately rejected p3 promise if even one promise reject
+  //This is an resolve value
 })
 
-
+Promise.reject("This is an rejected value").catch((value)=>{
+  console.log(value)
+  //This is an rejected value
+})
 
 

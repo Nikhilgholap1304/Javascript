b616b6d63cc02ff2f5bd4008def849ec244d4fc1
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-11-01 13:14:33 +0530

    update

--- Extra/prac25.js
+++ Extra/prac25.js
@@ -1,3 +1,11 @@
 // Async and Await
 
 // There’s a special syntax to work with promises in a more comfortable fashion, called “async/await”. It’s surprisingly easy to understand and use.
+
+async function f() {
+  return 1
+}
+
+console.log(f())
+//Promise { 1 }
+//All the way till now despite doing many projects you have been having no idea about this, how lame   

8fcd066539c21114b2dd8ca23cac4c868346595c
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-11-02 07:48:09 +0530

    update

--- Extra/prac27.js
+++ Extra/prac27.js
@@ -1,23 +1,23 @@
 //fetch Object
 
 //e.g
-const getAllUsers = async () => {
-  try {
+// const getAllUsers = async () => {
+//   try {
 
-    const res = await fetch('https://api.jikan.moe/v4/seasons/2005/spring?sfw')
-    //fetch will return an promise fulfilled one (either resolved or rejected) with respective value
-    // hence we have to assign await to wait before moving forward (let it produce then consume)
+//     const res = await fetch('https://api.jikan.moe/v4/seasons/2005/spring?sfw')
+//     //fetch will return an promise fulfilled one (either resolved or rejected) with respective value
+//     // hence we have to assign await to wait before moving forward (let it produce then consume)
 
-    const data = await res.json() //it also returns an promise, which means it will take time and we need to assign await to it or else the further code will execute directly without any value, like how we used to return a promise inside the .then() and then only further .then() will execute
+//     const data = await res.json() //it also returns an promise, which means it will take time and we need to assign await to it or else the further code will execute directly without any value, like how we used to return a promise inside the .then() and then only further .then() will execute
 
-    console.log(Object.keys(data))
+//     console.log(Object.keys(data))
 
-  } catch (error) {
-    console.log(error)
-  }
-}
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
-getAllUsers()
+// getAllUsers()
 
 // .json()
 // It returns a promise which resolves with the result of parsing the body text as JSON
@@ -25,17 +25,17 @@ getAllUsers()
 
 // here we have to use the try and catch block to handle the rejections/exceptions otherwise we can use .catch method
 
-console.log(fetch)
+// console.log(fetch)
 //[AsyncFunction: fetch] which returns promise as a response
 
 fetch('https://api.jikan.moe/v4/seasons/2005/spring?sfw')
-  .then((res) => {
-    return res
-  }).then((data) => {
+.then((res) => {
+    return res.json() //without using .json you won't be able to access the object since its not an JS object
+}).then((data) => {
     console.log(Object.keys(data))
-  }).catch((err) => {
+}).catch((err) => {
     console.log(err)
-  })
+})
 
 
 

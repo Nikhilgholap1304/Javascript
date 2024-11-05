b1f9a37f8de575218fd65f47529b02ecc864bce3
Nikhilgholap1304 <nikhilgholap1304@gmail.com>
2024-11-05 15:40:06 +0530

    update

--- Timeline/Day29(events)/index.js
+++ Timeline/Day29(events)/index.js
@@ -21,15 +21,27 @@ let div = document.querySelector(".container");
 // !using html event attribute
 // here you need to manually pass (event object not working though) this which represents target itself as an argument from onClick attribute
 
-let handleClick = (target) => {
-  console.log("hello");
-  console.log(target);
+// let handleClick = (target) => {
+//   console.log("hello");
+//   console.log(target);
+//   div.style.background = "#111";
+//   div.style.color = "#fff";
+// }
+
+
+// div.addEventListener("mouseover", function(e){
+//   console.log(this) //!target element incase its an function
+//   console.log(e); //e is now an Mouse Event object
+//   div.style.background = "#111";
+//   div.style.color = "#fff";
+// })
+
+div.addEventListener("mouseout", function(e){
+  console.log(this) //!target element incase its an function
+  console.log(e); //e is now an Mouse Event object
   div.style.background = "#111";
   div.style.color = "#fff";
-}
-
-
-
+})
 
 
 
